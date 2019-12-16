fbbabeff9ec012fa5516cd2e3dfa8633fa184d39
mohammad <chatcart3@gmail.com>
2019-12-16 21:58:33 +0330

    git commite fragment library add f

--- managerpermission/src/main/java/com/kotlin/managerpermission/base/ManagerPermissionFragment.kt
+++ managerpermission/src/main/java/com/kotlin/managerpermission/base/ManagerPermissionFragment.kt
@@ -2,23 +2,31 @@ package com.kotlin.managerpermission.base
 
 import android.app.AlertDialog
 import android.content.pm.PackageManager
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.requestPermissions
+import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
 import androidx.core.content.ContextCompat
-import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.Fragment
 
 
-class ManagerPermissionFragment(val fragment: FragmentActivity, val list: List<String>, val code: Int) {
+class ManagerPermissionFragment(val fragment: Fragment, val list: List<String>, val code: Int):Fragment() {
+
+
     fun checkpermission() {
         if (isPermissionGranted() != PackageManager.PERMISSION_GRANTED)
             showDialg()
         else
-            Toast.makeText(fragment, "Permissions already granted", Toast.LENGTH_SHORT).show()
+            Toast.makeText(fragment.context, "Permissions already granted", Toast.LENGTH_SHORT).show()
         isPermissionGranted()
     }
 
     private fun showDialg() {
-        val builder = AlertDialog.Builder(fragment)
+        val builder = AlertDialog.Builder(fragment.context)
         builder.setTitle("Need permission accept")
         builder.setMessage("Some permissions are required to do the task.")
         builder.setPositiveButton("ok", { dialog, which -> requestPermission() })
@@ -29,16 +37,17 @@ class ManagerPermissionFragment(val fragment: FragmentActivity, val list: List<S
 
     private fun requestPermission() {
         val permission = deniedPermission()
-        if (ActivityCompat.shouldShowRequestPermissionRationale(fragment, permission))
-            Toast.makeText(fragment, "Should show an explanation.", Toast.LENGTH_LONG).show()
+        if (shouldShowRequestPermissionRationale(fragment.activity!!.parent, permission))
+            Toast.makeText(fragment.context, "Should show an explanation.", Toast.LENGTH_LONG).show()
         else
-            ActivityCompat.requestPermissions(fragment, list.toTypedArray(), code)
+            requestPermissions(fragment.activity!!.parent, list.toTypedArray(), code)
+
     }
 
     private fun deniedPermission(): String {
         for (permission in list) {
             if (ContextCompat.checkSelfPermission(
-                    fragment,
+                    fragment.context!!.applicationContext,
                     permission
                 ) == PackageManager.PERMISSION_DENIED
             )
@@ -50,7 +59,7 @@ class ManagerPermissionFragment(val fragment: FragmentActivity, val list: List<S
     private fun isPermissionGranted(): Int {
         var Counter = 0
         for (permission in list) {
-            Counter += ContextCompat.checkSelfPermission(fragment, permission)
+            Counter += ContextCompat.checkSelfPermission(fragment.context!!.applicationContext, permission)
         }
         return Counter
     }
